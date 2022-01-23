389e1f18702a4bed143541cdd1f8b7ed2f6944e5
gerarsim <75917703+gerarsim@users.noreply.github.com>
2022-01-23 10:53:19 -0500

    service

--- variables.tf
+++ variables.tf
@@ -9,3 +9,15 @@ variable "cluster_name" {
   description = "The name of AWS ECS cluster"
   default = "akssmerdoud"
 }
+variable "region" {
+  default = "ca-central-1"
+}
+variable "main_vpc_cidr" {
+  default = "10.0.0.0/24"
+}
+variable "public_subnets" {
+  default = "10.0.0.128/26"
+}
+variable "private_subnets" {
+  default = "10.0.0.192/26"
+}
